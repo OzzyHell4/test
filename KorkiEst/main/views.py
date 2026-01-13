f817e9dfb875c8bb44d289a2a551cbc8d183bc59
OzzyHell4 <101122544+OzzyHell4@users.noreply.github.com>
2026-01-14 01:34:18 +0300

    сделал файл base

--- KorkiEst/main/views.py
+++ KorkiEst/main/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Course, ProgLang
 
-# Create your views here.
+
+def product_list(request, progLang_slug=None):
+    progLangs = ProgLang.objects.all()
+    courses = Course.objects.all()
+
+    progLang = None
+    if progLang_slug:
+        progLang = get_object_or_404(ProgLang, progLang_slug=progLang_slug)
+        courses = courses.filter(progLang=progLang)
+
+    return render(request, 'main/course/list.html', {'progLang':progLang, 
+                                                     'progLangs':progLangs, 
+                                                     'courses':courses})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Course, id=id, slug=slug)
+    related_products = Course.objects.filter(progLang=product.progLang).exclude(id=product.id)[:4]
+
+    return render(request, 'main/course/detail.html', {'product': product,
+                                                       'related_products':related_products})
